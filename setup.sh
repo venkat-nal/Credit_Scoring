2ca892de33c0983a5bd89f9f2944633b363cf111
venkat-nal <77226878+venkat-nal@users.noreply.github.com>
2022-08-04 14:47:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,8 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
+echo "[server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
+enableCORS = false\"n\
+"> ~/.streamlit/config.toml
